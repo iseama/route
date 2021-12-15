046d24e086cd4305e2f125e01df33a26a1f9c4bf
iseama <96161627+iseama@users.noreply.github.com>
2021-12-15 13:02:38 +0700

    Update jpstatic.js

--- jpstatic.js
+++ jpstatic.js
@@ -1,1 +1,1 @@
-<script>(function(){let id=location.pathname.split('/').slice(-1)[0];location.replace('https://jp.isolasi.xyz/'+id+'');})();</script>
+<script>(function(){let id = location.pathname.split('/').slice(-1)[0];location.replace('https://jp.isolasi.xyz/'+id+'');})();</script>
