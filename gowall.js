0a5eb2d80005a6ba2515dc07a4b20cc383d0b7c2
iseama <96161627+iseama@users.noreply.github.com>
2021-12-15 15:43:11 +0700

    Create gowall.js

---
+++ gowall.js
@@ -0,0 +1,8 @@
+(function(){
+	let id = location.pathname.split('/').slice(-1)[0];
+    let mbot = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
+    if(id) {
+        if(mbot) location.replace('https://wall.isolasi.xyz/'+id+'');
+        else location.href = 'https://wall.isolasi.xyz/'+id+'';
+    }
+})();
